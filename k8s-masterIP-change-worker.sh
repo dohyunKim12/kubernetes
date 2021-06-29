f810f153ca25051db86ac1e1d31302701560a0ef
dohyunkimi12 <kdh951102@gmail.com>
2021-06-29 01:18:04 +0000

    Update k8s-masterIP-change-worker.sh
    change sed command to using wildcard
    
    Signed-off-by: dohyunkimi12 <kdh951102@gmail.com>

--- k8s-masterIP-change-worker.sh
+++ k8s-masterIP-change-worker.sh
@@ -1,13 +1,11 @@
 #! /bin/bash
 
-echo -n "Enter Master's OLD IP: "
-read oldip
 echo -n "Enter Master's NEW IP: "
 read newip
 
 
-sed -i "s/    server: https:\/\/$oldip:6443/    server: https:\/\/$newip:6443/" /etc/kubernetes/kubelet.conf
-sed -i "s/    server: https:\/\/$oldip:6443/    server: https:\/\/$newip:6443/" /etc/kubernetes/bootstrap-kubelet.conf
+sed -i "s/    server: https:.*/    server: https:\/\/$newip:6443/" /etc/kubernetes/kubelet.conf
+sed -i "s/    server: https:.*/    server: https:\/\/$newip:6443/" /etc/kubernetes/bootstrap-kubelet.conf
 
 systemctl restart kubelet
 
